d1beb18348fcaf041dc33877c9b3018d47babf2a
Rafael Momberg de Andrade <rafahell.momberg@gmail.com>
2021-06-01 16:26:01 -0300

    Criado o repository e a interface PostagemRepository

---
+++ centraRpgl/src/main/java/br/com/hfdev/centraRpgl/repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package br.com.hfdev.centraRpgl.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.hfdev.centraRpgl.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+}
