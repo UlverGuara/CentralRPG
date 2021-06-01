6b7e823fab828799f90c4cc0467d4610de1d0c91
Rafael Momberg de Andrade <rafahell.momberg@gmail.com>
2021-06-01 15:55:16 -0300

    Iniciando o projeto

---
+++ centraRpgl/src/main/java/br/com/hfdev/centraRpgl/CentraRpglApplication.java
@@ -0,0 +1,13 @@
+package br.com.hfdev.centraRpgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentraRpglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentraRpglApplication.class, args);
+	}
+
+}
---
+++ centraRpgl/src/test/java/br/com/hfdev/centraRpgl/CentraRpglApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hfdev.centraRpgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentraRpglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
