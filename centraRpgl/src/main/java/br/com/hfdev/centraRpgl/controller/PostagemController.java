b00fbe233f6828416b85745ae5e3b8f6806d9140
Rafael Momberg de Andrade <rafahell.momberg@gmail.com>
2021-06-01 16:48:43 -0300

    Adicionando a anotação @Autowired e utilizando o metodo findAll no PostagemController

--- centraRpgl/src/main/java/br/com/hfdev/centraRpgl/controller/PostagemController.java
+++ centraRpgl/src/main/java/br/com/hfdev/centraRpgl/controller/PostagemController.java
@@ -1,10 +1,15 @@
 package br.com.hfdev.centraRpgl.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.hfdev.centraRpgl.model.Postagem;
 import br.com.hfdev.centraRpgl.repository.PostagemRepository;
 
 @RestController @RequestMapping("/postagens") @CrossOrigin ("*")
@@ -12,6 +17,11 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
 	
 	
 
